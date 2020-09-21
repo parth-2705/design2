30cf87bf87b360ce6a42bfa7a865dfd0b49f41c1
parth-2705 <40501175+parth-2705@users.noreply.github.com>
2020-09-21 16:26:13 +0530

    Add files via upload

--- dist/modal.js
+++ dist/modal.js
@@ -1,5 +1,6 @@
-$(window).load(function(){
-   setTimeout(function(){
-       $("#myModal").show();
-   }, 2000);
+$(document).ready(function() {
+setTimeout(function() { $( "#myModal" ).show(); }, 20000);
+$(".cut").click(function(){
+  $("#myModal").hide();
 });
+})
